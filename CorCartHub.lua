d161c2fb41ca52af4fb3170d3a8aff3e63abe393
Cortex <151866914+anilenzo@users.noreply.github.com>
2024-07-02 20:02:35 +0300

    CorCartHub.lua

--- CorCartHub.lua
+++ CorCartHub.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "CortexHub(Create a Cart Ride)", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 local Tab = Window:MakeTab({
 	Name = "Main",
